798405e36a35ed5519799c1a1b38bdada087b837
lilwoodb <35696010+lilwoodb@users.noreply.github.com>
2019-10-02 22:20:17 -0500

    Homework 6 - Minitest

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,14 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test "should get ask" do
+    get static_pages_ask_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get about" do
-    get static_pages_about_url
-    assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-  end
-  
 end
