bd3a60dadf58167dad185de089b0ceaaf90c45ee
lilwoodb <woodburn@grinnell.edu>
2019-10-16 10:29:58 -0500

    Revert "work from 10.1-10.3 with errors"
    This reverts commit 0a52868f29c40b518141b8d98bcfcb2364a68b31.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,5 +6,5 @@ class User < ApplicationRecord
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
