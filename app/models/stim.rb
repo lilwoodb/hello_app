f93bc6575e635fb1c0fe5263bdec59a531f19e82
lilwoodb <woodburn@grinnell.edu>
2019-11-11 23:24:28 -0600

    Finetuning for HW 10

--- app/models/stim.rb
+++ app/models/stim.rb
@@ -1,6 +1,5 @@
 class Stim < ApplicationRecord
-  before_save { self.name = name.downcase }
-  validates :name,  presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+  validates :name,  presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: true }
   validates :description, presence: true, length: { maximum: 255 }
   validates :source, presence: true, length: { maximum: 255 }
 end
